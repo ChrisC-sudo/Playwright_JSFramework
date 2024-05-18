c8c433d30409d8953570b2c81130dc57bea91e66
Chris Christie <cchristie035@gmail.com>
2024-05-18 16:46:35 -0400

    play setup

---
+++ tests/login.spec.js
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+
+test('Login Test', async function({browser}){
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    const userName = page.locator('input#userEmail');
+    const userPass = page.locator('input#userPassword');
+    const submit = page.locator('input.btn');
+    await page.goto("https://rahulshettyacademy.com/client/");
+    console.log(await page.title());
+    await userName.fill(process.env.userN);
+    await userPass.fill(process.env.userP);
+    await submit.click();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// input#userEmail //input#userPassword //input.btn
+
+
+
+//const {test} = require('@playwright/test')
