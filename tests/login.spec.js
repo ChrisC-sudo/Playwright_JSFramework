7f683864131673cd7f47bb1afc70fb61a8a5a76c
Chris Christie <cchristie035@gmail.com>
2024-05-18 17:32:10 -0400

    sampling

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -7,7 +7,7 @@ test('Login Test', async function({browser}){
     const userName = page.locator('input#userEmail');
     const userPass = page.locator('input#userPassword');
     const submit = page.locator('input.btn');
-    await page.goto("https://rahulshettyacademy.com/client/");
+    await page.goto("https://website.com");
     console.log(await page.title());
     await userName.fill(process.env.userN);
     await userPass.fill(process.env.userP);
