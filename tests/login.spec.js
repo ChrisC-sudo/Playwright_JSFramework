31eab6dffcade575ce5a8eb81d6ea05836860c2b
Chris Christie <cchristie035@gmail.com>
2024-05-21 15:07:55 -0400

    env resolved

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,42 +1,22 @@
 import { test, expect } from '@playwright/test';
 require('dotenv').config();
 
-async function locate(){
-    const selectors= {
-        USERN:'input#userEmail',
-        USERP:'input#userPassword',
-        SUBMIT:'input.btn'
-    }
+async function locate() {
+    const selectors = {
+        USERN: 'input[name="user[email]"]', 
+        USERP: 'input[name="user[password]"]', 
+        SUBMIT: 'button[type="submit"]' 
+    };
     return selectors;
 }
 
-test('Login Test', async function({browser}){
-
+test('Login Test', async ({ browser }) => {
     const context = await browser.newContext();
     const page = await context.newPage();
     const selectors = await locate();
-    await page.goto("https://courses.ultimateqa.com/users/sign_in");
+    await page.goto(process.env.URL);
     console.log(await page.title());
-    await page.fill(selectors.USERN,process.env.USERN);
-    await page.fill(selectors.USERP,process.env.USERP);
+    await page.fill(selectors.USERN, process.env.USERN);
+    await page.fill(selectors.USERP, process.env.USERP);
     await page.click(selectors.SUBMIT);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// input#userEmail //input#userPassword //input.btn
-
-
-
-//const {test} = require('@playwright/test')
