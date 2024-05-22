218fadf4f122bab42ab54f874e05a7395021f5c0
Chris Christie <cchristie035@gmail.com>
2024-05-22 13:13:17 -0400

    coverage plus new test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,11 +1,18 @@
 import { test, expect } from '@playwright/test';
+import exp from 'constants';
 require('dotenv').config();
 
 async function locate() {
     const selectors = {
+        WELCOME : '.page__heading',
         USERN: 'input[name="user[email]"]', 
         USERP: 'input[name="user[password]"]', 
-        SUBMIT: 'button[type="submit"]' 
+        SUBMIT: 'button[type="submit"]',
+        FORGOT_PASS:'.form__forgot-password',
+        FORGOT_PASS_SUBMIT :'input[value="Submit"]',
+        RESET_PASSWORD_HEADING :'.password-reset__heading',
+        REMEMBER_ME :"input[type='checkbox']",
+        WEBSITE_NAME : 'h1.sr-only'
     };
     return selectors;
 }
@@ -15,8 +22,36 @@ test('Login Test', async ({ browser }) => {
     const page = await context.newPage();
     const selectors = await locate();
     await page.goto(process.env.URL);
+
+    const WEBURL = await page.locator(selectors.WEBSITE_NAME);
+    const WebName = await WEBURL.textContent();
+    expect(WebName.trim()).toEqual("UltimateQA")
     console.log(await page.title());
+
+    const welcomeElement = await page.locator(selectors.WELCOME);
+    const textContent = await welcomeElement.textContent();
+    expect(textContent.trim()).toContain("Welcome"); 
+
     await page.fill(selectors.USERN, process.env.USERN);
     await page.fill(selectors.USERP, process.env.USERP);
+    await page.click(selectors.REMEMBER_ME);
     await page.click(selectors.SUBMIT);
+});
+
+test('Forgot password test', async ({browser}) =>{
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    const selectors = await locate();
+    await page.goto(process.env.URL);
+
+    await page.click(selectors.FORGOT_PASS);
+    await page.fill(selectors.USERN, process.env.USERN);
+    await page.click(selectors.FORGOT_PASS_SUBMIT);
+
+    const ResetPassword =await page.locator(selectors.RESET_PASSWORD_HEADING)
+    const textContent = await ResetPassword.textContent();
+    expect(textContent.trim()).toContain("Help")
+    page.pause();
+
 });
